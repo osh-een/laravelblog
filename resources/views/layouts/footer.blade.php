fba43fff27d5ce0486535196a39a58f0917fa796
osh-een <oisinbell09@gmail.com>
2025-03-22 13:39:32 +0000

    refactored footer (footer.balde.php)

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,93 +1,64 @@
-<footer class="bg-gray-800 py-20 mt-20">
-    <div class="sm:grid grid-cols-3 w-4/5 pb-10 m-auto border-b-2 border-gray-700">
+<footer class="bg-blue-900 text-white py-16 mt-20 relative overflow-hidden">
+    <div class="sm:grid grid-cols-3 w-4/5 pb-10 m-auto border-b-2 border-blue-700">
         <div>
-            <h3 class="text-l sm:font-bold text-gray-100">
-                Pages
-            </h3>
-
-            <ul class="py-4 sm:text-s pt-4 text-gray-400">
-                <li class="pb-1">
-                    <a href="/">
-                        Home
-                    </a>
-                </li>
-                <li class="pb-1">
-                    <a href="/blog">
-                        Blog
-                    </a>
-                </li>
-                <li class="pb-1">
-                    <a href="/login">
-                        Login
-                    </a>
-                </li>
-                <li class="pb-1">
-                    <a href="/register">
-                        Register
-                    </a>
-                </li>
+            <h3 class="text-xl font-bold text-white">Explore</h3>
+            <ul class="py-4 text-lg text-blue-300 space-y-2">
+                <li><a href="/" class="footer-link">Home</a></li>
+                <li><a href="/blog" class="footer-link">Blog</a></li>
+                <li><a href="/login" class="footer-link">Login</a></li>
+                <li><a href="/register" class="footer-link">Register</a></li>
             </ul>
         </div>
 
         <div>
-            <h3 class="text-l sm:font-bold text-gray-100">
-                Find Us
-            </h3>
-
-            <ul class="py-4 sm:text-s pt-4 text-gray-400">
-                <li class="pb-1">
-                    <a href="/">
-                        What we do
-                    </a>
-                </li>
-                <li class="pb-1">
-                    <a href="/">
-                        Address
-                    </a>
-                </li>
-                <li class="pb-1">
-                    <a href="/">
-                        Phone
-                    </a>
-                </li>
-                <li class="pb-1">
-                    <a href="/">
-                        Contact
-                    </a>
-                </li>
+            <h3 class="text-xl font-bold text-white">Find Us</h3>
+            <ul class="py-4 text-lg text-blue-300 space-y-2">
+                <li><a href="/" class="footer-link">Our Mission</a></li>
+                <li><a href="/" class="footer-link">Contact</a></li>
+                <li><a href="/" class="footer-link">Volunteer</a></li>
+                <li><a href="/" class="footer-link">Donate</a></li>
             </ul>
         </div>
 
         <div>
-            <h3 class="text-l sm:font-bold text-gray-100">
-                Latest posts
-            </h3>
-
-            <ul class="py-4 sm:text-s pt-4 text-gray-400">
-                <li class="pb-1">
-                    <a href="/">
-                        Why we love tech
-                    </a>
-                </li>
-                <li class="pb-1">
-                    <a href="/">
-                        Why we love design
-                    </a>
-                </li>
-                <li class="pb-1">
-                    <a href="/">
-                        Why to use Laravel
-                    </a>
-                </li>
-                <li class="pb-1">
-                    <a href="/">
-                        Why PHP is the best
-                    </a>
-                </li>
+            <h3 class="text-xl font-bold text-white">Latest Articles</h3>
+            <ul class="py-4 text-lg text-blue-300 space-y-2">
+                <li><a href="/" class="footer-link">Saving Coral Reefs</a></li>
+                <li><a href="/" class="footer-link">Deep Sea Exploration</a></li>
+                <li><a href="/" class="footer-link">The Future of Marine Life</a></li>
+                <li><a href="/" class="footer-link">How You Can Help</a></li>
             </ul>
         </div>
     </div>
-    <p class="w-25 w-4/5 pb-3 m-auto text-xs text-gray-100 pt-6">
-        Copyright 2017-2021 Code With Dary. All Rights Reserved
-    </p>
+
+    <div class="w-4/5 m-auto flex flex-col md:flex-row justify-between items-center text-center md:text-left pt-6">
+        <p class="text-sm text-blue-200">&copy; 2023 Guardians of the Blue. All Rights Reserved.</p>
+        <div class="flex space-x-4 mt-4 md:mt-0">
+            <a href="#" class="social-icon"><i class="fab fa-facebook-f"></i></a>
+            <a href="#" class="social-icon"><i class="fab fa-twitter"></i></a>
+            <a href="#" class="social-icon"><i class="fab fa-instagram"></i></a>
+            <a href="#" class="social-icon"><i class="fab fa-youtube"></i></a>
+        </div>
+    </div>
 </footer>
+
+<style>
+    .footer-link {
+        transition: color 0.3s ease;
+    }
+
+    .footer-link:hover {
+        color: #ffffff;
+    }
+
+    .social-icon {
+        background-color: rgba(255, 255, 255, 0.2);
+        padding: 10px;
+        border-radius: 50%;
+        transition: background-color 0.3s ease;
+    }
+
+    .social-icon:hover {
+        background: rgba(255, 255, 255, 0.5);
+    }
+</style>
